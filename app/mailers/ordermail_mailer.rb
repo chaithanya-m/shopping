ed6012927b18f0f4eff64f555682919f49d69c0e
chaithanya <chaithanyamadduru03@gmail.com>
2022-12-04 11:51:15 +0530

    active job is working

--- app/mailers/ordermail_mailer.rb
+++ app/mailers/ordermail_mailer.rb
@@ -2,6 +2,8 @@ class OrdermailMailer < ApplicationMailer
 	 default from: 'chaithanyamadduru123@gmail.com'
 
   def orderPlaced_email
+
+    # binding.break
     @order = params[:order]
     @url  = 'http://localhost:3000/order_items'
     mail(to: @order.user.email, subject: 'Your order is placed')
