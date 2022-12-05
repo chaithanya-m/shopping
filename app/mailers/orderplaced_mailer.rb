f6cd617d1864240c2be94896cdb63c055f9263bf
chaithanya <chaithanyamadduru03@gmail.com>
2022-12-05 11:33:07 +0530

    mergit 1

---
+++ app/mailers/orderplaced_mailer.rb
@@ -0,0 +1,2 @@
+class OrderplacedMailer < ApplicationMailer
+end
---
+++ test/mailers/orderplaced_mailer_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class OrderplacedMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
